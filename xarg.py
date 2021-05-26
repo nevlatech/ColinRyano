90d8e62413cc75840ea832e4c8440082accfea9d
nevlatech <rengxi23@yeah.net>
2021-05-27 06:52:27 +0800

    initial commit

---
+++ xarg.py
@@ -0,0 +1,94 @@
+import sublime, sublime_plugin, re, os, os.path, mmap
+
+class XargCommand(sublime_plugin.TextCommand):
+
+
+	def getFields(self, lines):
+		for line in lines:
+			yield line.split(",")
+
+
+	def notEmpty(self, line):
+		if line not in ['\n', '\r\n', '']:
+			return line
+
+
+	def findFiles(self, path=sublime.packages_path(), type=".sublime-snippet"):
+		for root, dirs, files in os.walk(path):
+		    for file in files:
+		        if file.endswith(type):
+		             yield os.path.join(root, file)
+
+
+	def matchFile(self, path, pattern):
+		f = open(path)
+		s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+		if s.find(pattern.encode('utf-8')) != -1:
+			return s.read(s.size()).decode("utf-8")
+
+
+	def findSnippetContent(self, snippet):
+		return re.search(r'CDATA\[[\n\r]{0,2}(.*?)\]\]', snippet, re.DOTALL).group(1) if snippet else ''
+
+
+	def zipSnip(self, snippet, content):
+		for idx, field in enumerate(content):
+			snippet = re.sub(r'(?<!\\)\${{*{0}:*[a-zA-Z0-9]*}}*'.format(str(idx+1)) ,field, snippet)
+		snippet = re.sub(r'(?<!\\)\$\{\d+:(.+?)\}', '\\1', snippet)
+		return snippet
+
+
+	def findMatch(self, view, pattern, num):
+		return view.substr(view.find(pattern, num))
+
+
+	def getData(self):
+
+		data = {}
+
+		data['+metaRegion']     = self.view.find(r"(----)([\n\r])*(.*?[\n\r])*(====)(.+)([\n\r])*", 0)
+
+		data['asString']        = self.findMatch(self.view, r"(?<=----)(?<=[\n\r])*(.*?[\n\r])*(?=====)(?=.*?)(?=[\n\r])*", 0)
+
+		data['asLines']         = data['asString'] .splitlines()
+
+		data['asLinesMassaged'] = filter(self.notEmpty, data['asLines'])
+
+		return data
+
+
+	def getSnippet(self):
+		snippet = {}
+
+		snippet['name']             = self.findMatch(self.view, r"(?<=====)(.+)(?=[\n\r])*", 0)
+
+		snippet['match']            = '<tabTrigger>' + snippet['name']  + '</tabTrigger>'
+
+		snippet['filenames']        = list(self.findFiles())
+
+		snippet['matchedFiles']     = [self.matchFile(x, snippet['match']) for x in snippet['filenames']]
+
+		snippet['asString']         = [self.findSnippetContent(x) for x in snippet['matchedFiles']]
+
+		snippet['asStringMassaged'] = [re.sub(r'\r', '', content) for content in snippet['asString']]
+
+		return snippet
+
+
+	def run(self, edit):
+		window = sublime.active_window()
+		info = window.extract_variables()
+
+
+		data = self.getData()
+
+		snippet = self.getSnippet()
+
+		self.view.replace(edit, data['+metaRegion'], '')
+
+		for snippet in snippet['asStringMassaged']:
+			for fields in self.getFields(data['asLinesMassaged']):
+				snip = self.zipSnip(snippet,fields)
+				self.view.insert(edit, data['+metaRegion'].a, snip)
+
+		#self.view.insert(edit, 0, "Hello, World!")
