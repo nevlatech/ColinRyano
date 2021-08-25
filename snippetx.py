700128cf63043329739b843134c37b87d50fbd63
nevlatech <rengxi23@yeah.net>
2021-08-25 11:50:53 +0800

    xml node.text must convert to str explicitly

--- snippetx.py
+++ snippetx.py
@@ -27,23 +27,45 @@ class snippetxCommand(sublime_plugin.TextCommand):
     def findFiles(self, path, type=".sublime-snippet"):
 
         # return cache if exist
-        all_snippet_path = getattr(self, 'all_snippet_path', [])
-        if all_snippet_path:
-            return all_snippet_path
+        # all_snippet_path = getattr(self, 'all_snippet_path', [])
+        # if all_snippet_path:
+        #     return all_snippet_path
 
         # ... or setting cache
+        all_snippet_path = []
+        # paths = []
         for root, dirs, files in os.walk(path):
-            for file in files:
-                if file.endswith(type):
-                    all_snippet_path.append(os.path.join(root, file))
+            if '/.' in root or '/node_modules' in root:
+                continue
+            # paths.append(root)
+            for file_name in files:
+                if file_name.endswith(type):
+                    all_snippet_path.append(os.path.join(root, file_name))
+                    # if 'handlebars' in file_name:
+                    #     xml_root = ET.parse(os.path.join(root, file_name))
+                    #     t = xml_root.find('tabTrigger').text
+                    #     if str(t) == 'linkto':
+                    #         print(file_name)
+                    #         print("t: %s" % t)
         setattr(self, 'all_snippet_path', all_snippet_path)
+        # print("paths: %s" % paths)
         return all_snippet_path
 
     def xmlMatchTabTrigger(self, paths, trigger_name):
         for path in paths:
+            # try:
             xml_root = ET.parse(path)
-            if xml_root.find('tabTrigger').text == trigger_name:
-                yield xml_root
+            tabTrigger_node = xml_root.find('tabTrigger')
+            if tabTrigger_node is not None:
+                if str(tabTrigger_node.text) == trigger_name:
+                    # print("xml_root.find('tabTrigger').text: "+ tabTrigger_node.text)
+                    yield xml_root
+            else:
+                print("no tabTrigger_node")
+            # except Exception as e:
+            #     print("e: %s" % e)
+            #     print("path: %s" % path)
+            #     continue
 
     def zipSnip(self, snippet, fields, indent=''):
         snippet = snippet.strip()
@@ -64,19 +86,26 @@ class snippetxCommand(sublime_plugin.TextCommand):
         return False
 
     def filterByScope(self, snippet_xml, allowed):
+        scope_node = snippet_xml.find('scope')
+        if not scope_node:
+            return True
         scope_text = snippet_xml.find('scope').text
         scope_rmNeg = re.sub(r'- .*? ', '', scope_text)
         return self.checkScope(scope_rmNeg.split(', '), allowed)
 
-    def getData(self, patterns):
-        csv_lines = self.getMatch(patterns, 0).splitlines()
+    def getData(self, pattern):
+        csv_lines = self.getMatch(pattern, 0).splitlines()
+
+        assert csv_lines
+        print("csv_lines: %s" % csv_lines)
+        csv_lines = list(filter(lambda x: x.strip(), csv_lines))
         snippet_name = ''
         for i in [0, -1]:
             if 'sx:' in csv_lines[i]:
                 snippet_name = csv_lines.pop(i).split('sx:')[-1]
 
         return {
-            '+metaRegion': self.view.find(patterns, 0),
+            '+metaRegion': self.view.find(pattern, 0),
             'snippetName': snippet_name,
             'indent': re.findall(r'^[\t\s]*', csv_lines[0])[0],
             'asLinesMassaged': [
@@ -88,6 +117,7 @@ class snippetxCommand(sublime_plugin.TextCommand):
     def getSnippet(self, name=None, scope=['text.plain']):
         
         filenames = self.findFiles(sublime.packages_path())
+
         snippet_xmls = self.xmlMatchTabTrigger(filenames, name)
         snippet_contents = [
             x.find('content').text
